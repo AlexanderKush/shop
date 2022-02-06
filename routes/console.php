691942416dc51c93e7335c827e26d304f9265207
AlexKush <sahar63@yandex.ru>
2022-02-06 22:47:50 +0400

    finish step one

--- routes/console.php
+++ routes/console.php
@@ -18,7 +18,64 @@ use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('parseEkatalog', function () {
 
-    
+    function mb_str_replace($search, $replace, $string)
+    {
+        $charset = mb_detect_encoding($string);
+        $unicodeString = iconv($charset, "UTF-8", $string);
+        return str_replace($search, $replace, $unicodeString);
+    }
+
+    $url = 'https://www.e-katalog.ru/ek-list.php?katalog_=189&search_=rtx+3090';
+
+    $data = file_get_contents($url);
+
+    $dom = new DOMDocument();
+    @$dom->loadHTML($data);
+
+    $xpath = new DOMXPath($dom);
+
+    $totalProductsString = $xpath->query('//span[@class="t-g-q"]')[0]->nodeValue ?? false;
+    $totalProducts = preg_replace('/[^0-9]/', '', $totalProductsString);
+
+    $products = [];
+
+    function getProductPage() {
+
+        $divs = $xpath->query('//div[contains(@class, \'model-short-div\']');
+        
+    }
+
+    $divs = $xpath->query('//div[contains(@class, \'model-short-div\']');
+
+    $productsOnePage = $divs->length;
+
+    $totalPages = ceil($totalProducts / $productsOnePage);
+
+    foreach ($divs as $div) {
+        $a = $xpath->query('descendant::a[@class="model-short-title no-u"]', $div)[0];
+        $name = $a->nodeValue;
+
+        $price = '';
+        $range = $xpath->query('descendant::div[@class="model-price-range"]', $div)[0];
+        if ($range) {
+            $price =  mb_substr(trim(explode('Сра', $range->nodeValue)[0]), 1);
+            /* foreach ($range->childNodes as $child) {
+                if ($child->nodeName == 'a') {
+                    $price = trim($child->nodeValue);
+                }
+            } */
+            
+            dump($price);
+        }
+        $range = $xpath->query('descendant::div[@class="pr31 ib"]', $div)[0];
+        if ($range) {
+            $price = trim($range->nodeValue);
+        }
+    }
+
+    for ($i = 1; $i < $totalPages; $i++) {
+        $nextUrl = $url . '&page_=' . $i;
+    }
 
 });
 
