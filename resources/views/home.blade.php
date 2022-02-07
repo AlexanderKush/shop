3bc2f63847bdf72fa6c1c1f14c554f2e4df66b35
AlexKush <sahar63@yandex.ru>
2022-02-08 00:38:34 +0400

    CategoryProductFactory

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,37 @@
 @extends('layouts.app')
 
+@section('styles')
+<style>
+    .card {
+        height: 100%;
+    }
+    .card-img {
+        text-align: center;
+    }
+    .card-img img {
+        max-height: 100px;
+        width: auto;
+    }
+</style>
+@endsection
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<div class="row">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    @foreach ($categories as $category)
+    <div class="col-4 mb-4">
+        <div class="card">
+            <div class="card-img p-4 pb-0">
+                <img src="{{ asset('storage') }}/{{ $category->picture }}" class="card-img-top" alt="{{ $category->name }}">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">{{ $category->name }}</h5>
+                <p class="card-text">{{ $category->description }}</p>
+                <a href="{{ route('category', $category->id) }}" class="btn btn-primary">Перейти</a>
             </div>
         </div>
     </div>
+    @endforeach
+
 </div>
 @endsection
